0c6f7f33e211062abab5de8959e62b5dd71fd9c7
Josue Castillo <isaac9900.jc@gmail.com>
2023-04-17 15:05:16 -0400

    archivos css

---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+// const obtenerLatitudLongitud = () => {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(posicion => {
+//             let latitud = posicion.coords.latitude
+//             let longitud = posicion.coords.longitude
+//             console.log(latitud, longitud)
+//             obtenerClimaHora(latitud, longitud)
+//         })
+//     }else{
+//         console.log('hubo un error')
+//     }
+
+// }
+// obtenerLatitudLongitud();
+
+
+// const obtenerClimaHora = (latitud, longitud) => {
+//     const key = '2b305071c0ccb4ae2006870be1a1a3d5'
+
+//     console.log(latitud, longitud)
+// }
