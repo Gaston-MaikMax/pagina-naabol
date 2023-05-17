fe05e37a45176ce3273dd1bd146881d22047bcc5
Mike <gastoncondori97@gmail.com>
2023-05-17 15:45:12 -0400

    subir noticias

--- js/nav.js
+++ js/nav.js
@@ -10,10 +10,9 @@ btn.addEventListener('click', e => {
 
 /* eliminar*/
 
-
 function confirmarEliminacion() {
   Swal.fire({
-    title: 'Seguro quieres Dardebaja?',
+    title: 'Seguro quieres Dar debaja?',
     icon: 'warning',
     showCancelButton: true,
     confirmButtonColor: '#3085d6',
@@ -30,5 +29,62 @@ function confirmarEliminacion() {
   })
 }
  
-/*editar*/
+/*agregar*/
+async function crearUrse() {
+  const { value: formValues } = await Swal.fire({
+    title: 'Subir noticia',
+    input: 'text',
+    input: 'file',
+     inputAttributes: {
+   'accept': 'image/*',
+      'aria-label': 'Upload your profile picture'
+    },
+    html:
+      
+      '<input type:"file" id="swal-input1" class="swal2-input">' +
+      '<input id="swal-input2" class="swal2-input">',
+    focusConfirm: false,
+    preConfirm: () => {
+      return [
+        document.getElementById('swal-input1').value,
+        document.getElementById('swal-input2').value
+      ]
+    }
+  })
+
+  const { value: text } = await Swal.fire({
+  input: 'textarea',
+  inputLabel: 'Message',
+  inputPlaceholder: 'Type your message here...',
+  inputAttributes: {
+    'aria-label': 'Type your message here'
+  },
+  showCancelButton: true
+})
+
+  if (formValues) {
+    Swal.fire(JSON.stringify(formValues))
+  }
+}
+/*
+function crearUrse() {
+const { value: formValues } = await Swal.fire({
+  title: 'Multiple inputs',
+  html:
+    '<input id="swal-input1" class="swal2-input">' +
+    '<input id="swal-input2" class="swal2-input">',
+  focusConfirm: false,
+  preConfirm: () => {
+    return [
+      document.getElementById('swal-input1').value,
+      document.getElementById('swal-input2').value
+    ]
+  }
+})
+
+if (formValues) {
+  Swal.fire(JSON.stringify(formValues))
+}
+}
+*/
 
